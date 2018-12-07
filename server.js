87682225f7e4a2eac1c61bbff423907e557c641f
Nathan <naslee2@gmail.com>
2018-12-06 20:07:08 -0800

    Updated first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var session = require('express-session')
+var app = express();
+var bodyParser = require('body-parser');
+// var bcrypt =require('bcryptjs')
+
+app.use(express.static( __dirname + '/angular/dist' ));
+
+app.all("*", (req,res,next) => {
+    res.sendFile(path.resolve("./angular/dist/index.html"))
+});
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
